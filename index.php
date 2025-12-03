dd76cc08950021630681a9cc11293b6faaef9266
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-12-03 14:19:18 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jiniret</title>
+</head>
+<body>
+    <h2>Jiniret</h2>
+    <div style="text-align:center;">
+        <img src="jiniret.jpg" alt="jiniret" style="max-width:300px;height:auto;">
+    </div>
+<p>Jiniret first appeared in the animated version of the "God's Menu" music video on February 3, 2021, 
+    along the other SKZOO. 
+    The plush and figure versions were released starting from February 14.<p/>
+</body>
+</html>
